933adccde9b3e2a0aca4ef6ea9eac8789c796192
Julian Gindi <juliangindi@gmail.com>
2016-08-08 16:41:31 -0400

    initial commit

---
+++ Source/VR_Cornhole/VR_Cornhole.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VR_Cornhole.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VR_Cornhole, "VR_Cornhole" );
---
+++ Source/VR_Cornhole/VR_CornholeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "VR_CornholeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_CORNHOLE_API AVR_CornholeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
